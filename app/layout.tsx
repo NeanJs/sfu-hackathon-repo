0768a69e36abfdbb32315a87bfe49026476aeeee
uzayyildirim <uzayhq@gmail.com>
2025-10-04 21:16:38 -0700

    frontend/updated--landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ButtonBarProvider } from './components/button-bar/ButtonBarProvider'
 import ButtonBar from './components/button-bar/ButtonBar'
+import { SearchOverlayProvider } from './components/search-overlay/SearchOverlayProvider'
+import SearchOverlay from './components/search-overlay/SearchOverlay'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,10 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ButtonBarProvider>
-          {children}
-          <ButtonBar />
-        </ButtonBarProvider>
+        <SearchOverlayProvider>
+          <ButtonBarProvider>
+            {children}
+            <ButtonBar />
+            <SearchOverlay />
+          </ButtonBarProvider>
+        </SearchOverlayProvider>
       </body>
     </html>
   )
